c5df7255942ae4a56e0af0277e7bd173678cc2ee
Raj <Raj@DESKTOP-K3904CC>
2022-08-12 23:08:51 +0530

    Pattern

---
+++ pattern/src/pattern/pattern.java
@@ -0,0 +1,27 @@
+package pattern;
+
+public class pattern {
+	public static void main(String[] args) {
+		/*int rows=5;
+		for(int i=1;i<=rows;++i) {
+			for(int j=1;j<=i;++j) {
+				System.out.print("*");
+				}
+			System.out.println();
+			
+		}*/
+
+	int row=4;
+	for(int i=row;i>=1;--i) {
+		for(int j=1;j<=i;++j) {
+			System.out.print("*");
+			
+		}
+		System.out.println();
+	}
+	}
+	
+}
+	
+
+
