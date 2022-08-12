46b8c81dd7d175b42737d5a72219ddd6dee091dd
Raj <Raj@DESKTOP-K3904CC>
2022-08-12 22:56:51 +0530

    Find the Areaa

---
+++ Area/src/Areaa.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Areaa {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the Value Circel:");
+    System.out.println("Enter the Value Square:");
+    System.out.println("Enter the Value Rectangle:");
+    System.out.println("Enter your Choice:");
+   char choice=sc.next().charAt(0);
+   switch(choice) {
+   case 'a':
+	   System.out.println("Enter the redius of Circle:");
+	   double r=sc.nextDouble();
+	   double ca=(22/7.0)*r*r;
+	   System.out.println("Area of Circle:"+ca);
+	   break;
+   case 'b':
+	   System.out.println("Enter the redius of Square:");
+	   double n=sc.nextDouble();
+	   double s=n*n;
+	   System.out.println("Area of Square:"+s);
+	   break;
+   case 'c':
+	   System.out.println("Enter lenght of Rectangle:");
+	   double j=sc.nextDouble();
+	   System.out.println("Enter the breath of Rectangle:");
+	   double b=sc.nextDouble();
+	   double ra=j*b;
+	   System.out.println("Area of Rectangle:"+ra);
+	   break;
+	   default:
+		   System.out.println("Your Choice is Wrong!!");
+   }
+   }   
+   
+
+
+}
