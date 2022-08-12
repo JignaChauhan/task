d1da41b3a4781ea18fd8c66589580e516fcf4ed1
Raj <Raj@DESKTOP-K3904CC>
2022-08-12 23:00:15 +0530

    Array

---
+++ Array/src/Array.java
@@ -0,0 +1,20 @@
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Simple Create a Arrary
+		int[] num = {4,5,6,8};
+		System.out.println(num[3]);
+		//Access the Element of Array
+		String[] cars= {"nexon","baleno","kia"};
+		System.out.println(cars[0]);
+		//Change in Array element
+		String[] carss= {"Nexon","Baleno","KIA"};
+		carss[2]="Swift";
+        System.out.println(carss[2]);
+        //Array Lenght
+        System.out.println(carss.length);
+	}
+
+}
