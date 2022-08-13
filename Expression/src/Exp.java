93b884cef7482593189ad43389316e6fe029951e
Raj <Raj@DESKTOP-K3904CC>
2022-08-13 19:47:33 +0530

    math Expressions

---
+++ Expression/src/Exp.java
@@ -0,0 +1,18 @@
+
+public class Exp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=4,y=16,b=0,v=5,u=7,d=4;
+int ans=(x-y/2);
+int j=(b*b-(4*x*y)/(2*x));
+int n=v*v-u*u;
+int k=(x*y-d);
+
+System.out.println("Enter the value"+ans);
+System.out.println("Enter the value"+j);
+System.out.println("Enter the value"+n);
+System.out.println("Enter the value"+k);
+	}
+
+}
