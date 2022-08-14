04539d445b64b604ff0bf7ad7b7401dbdc32e4df
Raj <Raj@DESKTOP-K3904CC>
2022-08-14 20:45:10 +0530

    Grad encrypt by 10

---
+++ Expression/src/Grad.java
@@ -0,0 +1,17 @@
+public class Grad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//increment the grade
+	char grade='J';
+	char grade1='k';
+	grade=(char)(grade+10);
+	System.out.println(grade);
+	//Decrement the grade
+	grade1=(char)(grade1-10);
+	System.out.println(grade1);
+	
+
+	}
+
+}
