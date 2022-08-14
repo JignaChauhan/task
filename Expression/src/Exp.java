5c32d17639f7042f8d8927384bc34ec3eb6d18d7
Raj <Raj@DESKTOP-K3904CC>
2022-08-14 22:39:01 +0530

    Operators

--- Expression/src/Exp.java
+++ Expression/src/Exp.java
@@ -3,13 +3,16 @@ public class Exp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int x=4,y=16,b=0,v=5,u=7,d=4;
+int x=4,y=7,b=0,v=5,u=7,d=4;
 int ans=(x-y/2);
 int j=(b*b-(4*x*y)/(2*x));
 int n=v*v-u*u;
 int k=(x*y-d);
 int z=(++y*8);
-
+float a=(7/4.0f*9/2.0f);
+int m=(v*v-u*u)/2*x*y;
+System.out.println("Enter the value"+m);
+System.out.println("Enter the value"+a);
 System.out.println("Enter the value"+ans);
 System.out.println("Enter the value"+j);
 System.out.println("Enter the value"+n);
---
+++ operator/src/operator/operator.java
@@ -0,0 +1,63 @@
+package operator;
+
+public class operator {
+
+	public static void main(String[] args) {
+		//Arithmetic Operator
+		/*int sum1=30+27;
+		int sum2=sum1+30;
+		int sum3=sum2+sum1;
+		System.out.println(sum3);
+		int y=30;
+		++y;
+		System.out.println(y);
+
+		int x=30*27;
+		int x1=x*30;
+		int x2=x1*x1;
+		System.out.println(x2);
+		//Assignment Operator
+		int x3 =5;
+		 x3+=10;
+		 x3 -=10;
+		 x3 *=10;
+		 x3 /=10;
+		 x3 %=10;
+		 x3 &=10;
+		 x3 |=10;
+		 x3 <<=10;
+		 x3 >>=10;
+		 System.out.println(x3);
+		 //Logical operator
+		 int z=5;
+		System.out.println( z<5 && z<10);
+		System.out.println(z<5 || z<10);
+		System.out.println(!(z<5 && z<10));
+		//comparison operator
+        int N=30;
+        int J=27;
+        System.out.println(N == J);
+        System.out.println(N != J);
+        System.out.println(N > J);
+        System.out.println(N < J);
+        System.out.println(N >= J);
+        System.out.println(N<= J);
+		
+	}*/
+		int a=4;
+		int b=2;
+		int c=(++a);//++ used then increment the value +1
+		int d=(--a);//--decrement the vlaue -1
+		int c1=(a++);//that condition ++ is the value of (3++)answer is 3
+		int d1=(a--);//that condition -- is increment the value of a (3--)Answer is 4
+		int e=(a%b);
+		int f=(a=+b);
+		System.out.println("Enter the Ans:"+c);
+		System.out.println("Enter the Ans:"+d);
+		System.out.println("Enter the Ans:"+c1);
+		System.out.println("Enter the Ans:"+d1);
+		System.out.println("Enter the Ans:"+e);
+		System.out.println("Enter the Ans:"+f);
+	}
+}
+
