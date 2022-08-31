6ca5e0d1ac35bf58896437609c7418f490ad060c
Raj <Raj@DESKTOP-K3904CC>
2022-08-31 20:42:34 +0530

    Array

---
+++ Array/src/Array_3.java
@@ -0,0 +1,12 @@
+public class Array_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int arr[]=new int[] {1,2,3,4,5,6,7,8,9,10};
+System.out.println("Enter the Number:");
+for(int i=1;i<arr.length;i=i+2) {
+	System.out.println(arr[i]);
+}
+	}
+
+}
