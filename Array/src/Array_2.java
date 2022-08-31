c11698963a67f1f47940bc5e3b2c3881d76f875f
Raj <Raj@DESKTOP-K3904CC>
2022-08-31 20:32:57 +0530

    Array

---
+++ Array/src/Array_2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Array_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int[] arr=new int []{1,2,3,4,5};
+		//Scanner sc=new Scanner(System.in);
+		//int arr=sc.nextInt();
+ System.out.println("Enter Orignal Array:");
+ for(int i=0;i<arr.length;i++) {
+	 System.out.println(arr[i]+"");
+	 
+ }
+ System.out.println();
+ System.out.println("Enter the Reverse Array:");
+ for(int i=arr.length-1;i>=0;i--) {
+	 System.out.println(arr[i]+"");
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+	}
+
+}
