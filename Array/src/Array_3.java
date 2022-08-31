f97622e2150e63cc420d44a03a6a977a6d740903
Raj <Raj@DESKTOP-K3904CC>
2022-08-31 20:49:06 +0530

    Array

--- Array/src/Array_3.java
+++ Array/src/Array_3.java
@@ -1,12 +1,18 @@
+//Number of Even
 public class Array_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int arr[]=new int[] {1,2,3,4,5,6,7,8,9,10};
-System.out.println("Enter the Number:");
+System.out.println("The Even Number:");
 for(int i=1;i<arr.length;i=i+2) {
 	System.out.println(arr[i]);
 }
-	}
+System.out.println("The Odd Number:");
+int arrr[]=new int[] {1,2,3,4,5,6,7,8,9,10};
+for(int i=0;i<arr.length;i=i+2) {
+	System.out.println(arrr[i]);
+}
+}
 
 }
