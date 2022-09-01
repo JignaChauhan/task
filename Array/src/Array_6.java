960ba401641b84f1f8546189f04c9577fc53dfb2
Raj <Raj@DESKTOP-K3904CC>
2022-09-01 20:34:29 +0530

    Array

---
+++ Array/src/Array_6.java
@@ -0,0 +1,13 @@
+public class Array_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float[]marks= {4.5f,5.5f,6.5f,6.5f,7.5f};
+		float sum=0;
+		for(float element:marks) {
+			sum=sum+element;
+		}
+System.out.println("The value of Sum is:"+ sum);
+
+}
+}
