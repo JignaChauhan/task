83f75e850595b97ccf8413ca2cb8ed418c362377
Raj <Raj@DESKTOP-K3904CC>
2022-09-01 22:33:45 +0530

    Array

--- Array/src/Array_10.java
+++ Array/src/Array_10.java
@@ -2,14 +2,16 @@ public class Array_10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int []arr= {12,14,16,78,52,45,78};
-int max=0;
+int []arr= {12,14,16,78,52,45,78,1200,-2100};
+int max=Integer.MIN_VALUE;
 for(int e:arr) {
 	if(e>max) {
 		max=e;
 	}
 }
 System.out.println("The value of the maximum element in this arrys is:"+max);
+System.out.println(Integer.MIN_VALUE);
+System.out.println(Integer.MAX_VALUE);
 	}
 
 }
