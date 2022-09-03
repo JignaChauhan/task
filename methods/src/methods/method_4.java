79a8335d9c1c97bc7a3fbc73376f3f21d4968c5b
Raj <Raj@DESKTOP-K3904CC>
2022-09-03 21:58:36 +0530

    Method Program

--- methods/src/methods/method_4.java
+++ methods/src/methods/method_4.java
@@ -5,7 +5,7 @@ public class method_4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str=new String("Hi Welcome to my world!");
-		int count=0;
+		//int count=0;
 		for(int i=0;i<str.length();i++) {
 			if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'|str.charAt(i)=='o'||str.charAt(i)=='u')
 	      //count++;
